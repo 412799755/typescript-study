a170686f1276022d098c165a76e74d9da5b741c2
412799755 <412799755@qq.com>
2019-10-17 19:52:01 +0800

    function

---
+++ function/function.ts
@@ -0,0 +1,23 @@
+// Named function
+function add(x: number, y: number): number {
+    return x + y;
+}
+// Anonymous function
+let myAdd = function(x: number, y: number): number { return x + y; };
+
+let myAdd1: (x: number, y: number ) => number = function(x: number, y: number): number { return x + y; }
+//参数名可以不一样
+let myAdd2: (baseValue: number, increment: number) => number =
+    function(x: number, y: number): number { return x + y; };
+//按上下文归类,自动类型推论
+// The parameters `x` and `y` have the type number
+let myAdd3: (baseValue: number, increment: number) => number =
+    function(x, y) { return x + y; };
+
+// ？表示参数可传可不传
+function buildName(firstName: string, lastName?: string) {
+    if (lastName)
+        return firstName + " " + lastName;
+    else
+        return firstName;
+}
